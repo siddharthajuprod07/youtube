30614386a680903b8b9561df3e4a468146bbd6db
siddharthajuprod07 <siddhartha.juprod07@gmail.com>
2018-12-23 14:17:56 +0530

    code and data for my youtube video https://youtu.be/DdcAlS8IZJQ

---
+++ run_batch_from_dashboard/test.bat
@@ -0,0 +1,5 @@
+@echo off
+color 0a
+mode 1000
+
+echo %random%%random% > C:\Users\siddh\Desktop\test.txt
---
+++ run_batch_from_dashboard/test.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.call([r'D:\YouTube\SplunkHowTo\run_batch_file_from_dashboard\test.bat'])
