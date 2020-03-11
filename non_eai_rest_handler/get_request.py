29620722d598d8cae3b2c0f6f36a718657d85c09
techiesid <techiesid1985@gmail.com>
2020-03-11 09:35:02 +0530

    new changes

--- non_eai_rest_handler/get_request.py
+++ non_eai_rest_handler/get_request.py
@@ -1,4 +1,4 @@
 import requests
 
-r = requests.get('https://localhost:8089/services/movie_count_by_lang?ln=fr', auth=('admin', 'monitor!'),verify=False)
+r = requests.get('https://localhost:8089/services/movie_count_by_lang?ln=fr', auth=('admin', '<password>'),verify=False)
 print(r.text)
--- non_eai_rest_handler/post_request.py
+++ non_eai_rest_handler/post_request.py
@@ -1,5 +1,5 @@
 import requests
 
 data = {'somekey': 'somevalue'}
-r = requests.post('https://localhost:8089/services/movie_count_by_lang', auth=('admin', 'monitor!'),verify=False,data=data)
+r = requests.post('https://localhost:8089/services/movie_count_by_lang', auth=('admin', '<password>'),verify=False,data=data)
 print(r.text)
