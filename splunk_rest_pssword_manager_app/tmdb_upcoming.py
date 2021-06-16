958e52b6e75201db076cf5dc5bd9e86d7ec44166
Sid <techiesid1985@gmail.com>
2021-06-16 12:59:23 +0530

    Code used in youtube video https://youtu.be/WElklUqOTdM

---
+++ splunk_rest_pssword_manager_app/tmdb_upcoming.py
@@ -0,0 +1,93 @@
+import sys,os
+import json
+import requests as req
+import logging
+import logging.handlers
+import splunklib.client as client
+
+def setup_logger(level):
+     logger = logging.getLogger('tmdb_upcoming')
+     logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+     logger.setLevel(level)
+     file_handler = logging.handlers.RotatingFileHandler(os.environ['SPLUNK_HOME'] + '/var/log/splunk/tmdb_upcoming.log', maxBytes=25000000, backupCount=5)
+     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+     file_handler.setFormatter(formatter)
+     logger.addHandler(file_handler)
+     return logger
+ 
+logger = setup_logger(logging.INFO)
+
+
+def tmdb_api_call(requestURL,parameters):
+    response=req.get(url=requestURL,params=parameters)
+    if response.status_code !=200:
+        print('Status: ',response.status_code,'Headers: ',response.headers,'Error Response: ',response.json())
+        exit()
+    data=response.json()
+    return json.dumps(data)
+
+def get_upcoming_movies_by_page(api_key,page_number=1):
+    requestURL = "https://api.themoviedb.org/3/movie/upcoming"
+    #requestURL = "https://api.tmdb.org/3/movie/upcoming" #users in India please use this line of code for your requests
+    parameters = {"api_key":api_key,"page":page_number}
+    return tmdb_api_call(requestURL,parameters)
+
+def get_upcoming_movie_full_list(api_key):
+    results_fetched = True
+    page_number = 1
+    final_list = []
+    while results_fetched:
+        upcoming_movies = get_upcoming_movies_by_page(api_key,page_number)
+        data = json.loads(upcoming_movies)
+        final_list.append(data["results"])
+        results_fetched = bool(data["results"])
+        page_number += 1
+    return final_list
+
+def is_empty(file_path):
+    return os.stat(file_path).st_size==0
+
+def checkpoint(checkpoint_file,movie_id):
+    with open(checkpoint_file,'r') as file:
+        id_list = file.read().splitlines()
+        return (movie_id in id_list)
+
+def write_to_checkpoint_file(checkpoint_file,data):
+    with open(checkpoint_file,'a') as file:
+        file.writelines(data + "\n")
+
+
+def stream_to_splunk(checkpoint_file,data):
+    if checkpoint(checkpoint_file,str(data["id"])):
+        exit()
+    else:
+        write_to_checkpoint_file(checkpoint_file,str(data["id"]))
+        print(json.dumps(data))
+
+def get_credentials(sessionKey):
+    try:
+        service = client.connect(app="tmdb",owner="admin",token=sessionKey)
+        secrets = service.storage_passwords
+        for secret in secrets:
+            secret_content = secret.content
+            logger.info(secret_content)
+            api_key = secret_content["clear_password"]
+
+    except Exception as e:
+        raise Exception("No credentials have been found")
+    return api_key
+
+def main():
+    sessionKey = sys.stdin.readline().strip()
+    logger.info(sessionKey)
+    api_key = get_credentials(sessionKey)
+    #api_key='733ac994290c6f277b11565f26ebe1cb'
+    checkpoint_file = os.path.join(os.environ["SPLUNK_HOME"],'etc','apps','tmdb','bin','checkpoint','checkpoint.txt')
+    upcoming_movie_list = get_upcoming_movie_full_list(api_key)
+    for movie_list_by_page in upcoming_movie_list:
+        for movie in movie_list_by_page:
+            #data = json.dumps(movie)
+            stream_to_splunk(checkpoint_file,movie)
+
+if __name__ == "__main__":
+    main()
